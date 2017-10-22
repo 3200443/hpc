8183ca20f937c1afc9bed6233cf49f188cb4fafb
3200443 <thomas.boo@etu.upmc.fr>
2017-10-22 14:59:55 +0200

    test slack

--- morpho.c
+++ morpho.c
@@ -9,5 +9,17 @@
 
 void erosion3x3(uint8** entree,uint8 sortie,uint32 taille)
 {
+	int i = 0;
+	int j = 0;
 
+	for(i; i < taille; i++)
+	{
+		uint8 x1 = entree[i-1][j-1];
+		uint8 x2 = entree[i-1][j];
+		
+		for(int j = 0; j<taille; j++)
+		{
+
+		}
+	}
 }
