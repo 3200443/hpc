b83333148d2a22e830c5f10638c8a2fdd441ceb0
3200443 <thomas.boo@etu.upmc.fr>
2017-10-22 12:11:16 +0200

    ajout initial morpho

---
+++ morpho.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <ctype.h>
+#include <string.h>
+
+#include "nrdef.h"
+#include "nrutil.h"
+
+void erosion3x3(uint8** entree,uint8 sortie,uint32 taille)
+{
+
+}
