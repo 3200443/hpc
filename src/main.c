c546ac22a7ee0dfe3ecb3c575ad9155bc1fea8d6
3200443 <thomas.boo@etu.upmc.fr>
2017-11-02 12:21:36 +0100

    avec preprossessing comment

--- src/main.c
+++ src/main.c
@@ -3,13 +3,17 @@
 #include "test_mouvement.h"
 #include "test_mouvement_SSE2.h"
 
+#define OPTI 0
 
 int main(int argc, char* argv[])
 {
-
-    /*test_routine_FrameDifference(10);
+#if !OPTI
+    test_routine_FrameDifference(10);
     test_routine_sigmaDelta();
-*/
+#endif
+#if OPTI
     test_routine_FrameDifference_SSE2(10);
+
+#endif
     return 0;
 }
