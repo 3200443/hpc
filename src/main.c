253dd04785342394d3331cbf84b97bbee423573e
3200443 <thomas.boo@etu.upmc.fr>
2017-11-02 12:41:03 +0100

    correction d erreurs

--- src/main.c
+++ src/main.c
@@ -3,17 +3,14 @@
 #include "test_mouvement.h"
 #include "test_mouvement_SSE2.h"
 
-#define OPTI 0
-
+#define OPTI 1
 int main(int argc, char* argv[])
 {
-#if !OPTI
+#if OPTI == 1
+    test_routine_FrameDifference_SSE2(10);
+#else
     test_routine_FrameDifference(10);
     test_routine_sigmaDelta();
-#endif
-#if OPTI
-    test_routine_FrameDifference_SSE2(10);
-
 #endif
     return 0;
 }
--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "usage : ./test.sh ./[programme] [arg]"
+rm callg*
 valgrind --tool=callgrind  $1 $2
 kcachegrind &
