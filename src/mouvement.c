9fe3593bbedd6c595325c54b1cd5b42be6b05eab
Souleymane <souleymane936@gmail.com>
2017-11-09 23:49:02 +0100

    Fusion boucle SD+SDSIMD

--- src/mouvement.c
+++ src/mouvement.c
@@ -68,6 +68,58 @@ void routine_SigmaDelta_step0(uint8** I, uint8 **M, uint8 **V, long nrl, long nr
 void routine_SigmaDelta_1step(uint8 **It, uint8 **Itm1, uint8**Vt, uint8 **Vtm1, uint8**Mt, uint8 **Mtm1, uint8 **Et,  long nrl, long nrh, long ncl, long nch )
 {
 	uint8 **Ot = ui8matrix(nrl, nrh, ncl, nch);
+	uint8 tmpMtm1, tmpVtm1;
+    for(int i = nrl; i <= nrh; i++ ) //Step1 Mt Estimation
+    {
+    	for(int j = ncl; j <= nch; j++)
+    	{
+    		tmpMtm1 = Mtm1[i][j];
+    		tmpVtm1 = Vtm1[i][j];
+
+    		if(tmpMtm1 < It[i][j])
+    			Mt[i][j]  = tmpMtm1 + 1;
+
+    		else if(tmpMtm1 > It[i][j])
+    			Mt[i][j] = tmpMtm1 - 1;
+
+    		else
+    			Mt[i][j] = tmpMtm1;
+
+
+    		//Step 2 difference Computation
+    		Ot[i][j] = abs(Mt[i][j] - It[i][j]);
+
+
+    		//Step 3 Update and clamping
+    		if(tmpVtm1 < N * Ot[i][j])
+    			Vt[i][j] = tmpVtm1 + 1;
+
+    		else if(tmpVtm1 > N * Ot[i][j])
+    			Vt[i][j] = tmpVtm1 - 1;
+
+    		else
+    			Vt[i][j] = tmpVtm1;
+
+
+    		Vt[i][j] = max( min(Vt[i][j], VMAX), VMIN);
+
+
+    		//Step 4 Et estimation
+    		if(Ot[i][j] < Vt[i][j])
+    			Et[i][j] = 0;
+    		else
+    			Et[i][j] = 255;
+    	}
+    }
+
+    free_ui8matrix(Ot, nrl, nrh, ncl, nch);
+
+}
+
+void routine_SigmaDelta_1stepO(uint8 ** restrict It, uint8 ** restrict Itm1,uint8** restrict Vt, uint8 ** restrict Vtm1, 
+	uint8** restrict Mt, uint8 ** restrict Mtm1,uint8 ** restrict Et,  long nrl, long nrh, long ncl, long nch )
+{
+	uint8 **Ot = ui8matrix(nrl, nrh, ncl, nch);
     for(int i = nrl; i <= nrh; i++ ) //Step1 Mt Estimation
     {
     	for(int j = ncl; j <= nch; j++)
@@ -109,7 +161,7 @@ void routine_SigmaDelta_1step(uint8 **It, uint8 **Itm1, uint8**Vt, uint8 **Vtm1,
     			Vt[i][j] = Vtm1[i][j];
 
 
-    		Vt[i][j] = max( min(Vt[i][j], VMAX), VMIN);
+    		Vt[i][j] = maxO( minO(Vt[i][j], VMAX), VMIN);
 
 
     	}
@@ -124,71 +176,6 @@ void routine_SigmaDelta_1step(uint8 **It, uint8 **Itm1, uint8**Vt, uint8 **Vtm1,
     		else
     			Et[i][j] = 255;
     	}
-    }
-    free_ui8matrix(Ot, nrl, nrh, ncl, nch);
-
-}
-
-void routine_SigmaDelta_1stepO(uint8 ** restrict It, uint8 ** restrict Itm1,uint8** restrict Vt, uint8 ** restrict Vtm1, 
-     uint8** restrict Mt, uint8 ** restrict Mtm1,uint8 ** restrict Et,  long nrl, long nrh, long ncl, long nch )
-{
-    uint8 **Ot = ui8matrix(nrl, nrh, ncl, nch);
-    for(int i = nrl; i <= nrh; i++ ) //Step1 Mt Estimation
-    {
-        for(int j = ncl; j <= nch; j++)
-        {
-            if(Mtm1[i][j] < It[i][j])
-                Mt[i][j]  = Mtm1[i][j] + 1;
-
-            else if(Mtm1[i][j] > It[i][j])
-                Mt[i][j] = Mtm1[i][j] - 1;
-
-            else
-                Mt[i][j] = Mtm1[i][j];
-
-
-        }
-    }
-
-    for(int i = nrl; i <= nrh; i++)//Step 2 difference Computation
-    {
-        for(int j = ncl; j <= nch; j++)
-        {
-            Ot[i][j] = abs(Mt[i][j] - It[i][j]);
-        }
-    }
-
-
-    for(int i = nrl; i <= nrh; i++)//Step 3 Update and clamping
-    {
-        for(int j = ncl; j <= nch; j++)
-        {
-
-            if(Vtm1[i][j] < N * Ot[i][j])
-                Vt[i][j] = Vtm1[i][j] + 1;
-
-            else if(Vtm1[i][j] > N * Ot[i][j])
-                Vt[i][j] = Vtm1[i][j] - 1;
-
-            else
-                Vt[i][j] = Vtm1[i][j];
-
-
-            Vt[i][j] = maxO( minO(Vt[i][j], VMAX), VMIN);
-
-
-        }
-    }
-
-    for(int i = nrl; i <= nrh; i++)//Step 4 Et estimation
-    {
-        for(int j = ncl; j <= nch; j++)
-        {
-            if(Ot[i][j] < Vt[i][j])
-                Et[i][j] = 0;
-            else
-                Et[i][j] = 255;
-        }
     }
     free_ui8matrix(Ot, nrl, nrh, ncl, nch);
 
--- src/mouvement_SSE2.c
+++ src/mouvement_SSE2.c
@@ -18,7 +18,6 @@ void routine_FrameDifference_SSE2(vuint8 **It, vuint8 **Itm1, vuint8 **Et, long 
     vuint8 tmpIt;
     vuint8 tmpItm1;
     vuint8 tmpOt;
-    vuint8 pixelNoir = init_vuint8(0);
     vuint8 pixelBlanc = init_vuint8(255);
     vuint8 tmpEt;
     vuint8 maxSChar = init_vuint8(128);
@@ -33,8 +32,7 @@ void routine_FrameDifference_SSE2(vuint8 **It, vuint8 **Itm1, vuint8 **Et, long 
 
             //Si Ot < 0, on a 255, sinon 0 donc on inverse pour avoir 255 sur Et quand Ot>=0 et 0 pour Ot < 0
             vuint8 res = _mm_cmplt_epi8(_mm_sub_epi8(tmpOt, maxSChar), _mm_sub_epi8(seuil, maxSChar)); //Met 1 si inferieur au seuil et 0 si superieur
-            //display_vuint8(res," %d ", "Res");
-            //vuint8 dest = _mm_or_si128(_mm_and_si128(res, pixelNoir), _mm_andnot_si128(res, pixelBlanc)); //Pixel noir si res a 1 et pixel blanc si res a 0
+           
             res = _mm_andnot_si128(res, pixelBlanc);
             _mm_store_si128(&Et[i][j], res);
 
@@ -70,7 +68,6 @@ void routine_SigmaDelta_1stepSSE2(vuint8 **It, vuint8 **Itm1, vuint8**Vt, vuint8
     vuint8 tmpIt, tmpMt, tmpVt;
     vuint8 tmpItm1, tmpMtm1, tmpVtm1;
     vuint8 tmpOt;
-    vuint8 pixelNoir = init_vuint8(0);
     vuint8 pixelBlanc = init_vuint8(255);
     vuint8 tmpEt;
     vuint8 un = init_vuint8(1);
@@ -81,80 +78,60 @@ void routine_SigmaDelta_1stepSSE2(vuint8 **It, vuint8 **Itm1, vuint8**Vt, vuint8
     //255 devient 128, 128 => 0 et 0 => -128
     //Si on ne fait pas ca, on a 128 < 128 qui est faux
 
-    for(int i = vi0; i <= vi1; i++ )//Step1
+    for(int i = vi0; i <= vi1; i++ )
     {
         for(int j = vj0; j <= vj1; j++)
         {
 
-
+        	//Step1
             tmpMtm1 = _mm_load_si128(&Mtm1[i][j]);
             tmpIt = _mm_load_si128(&It[i][j]);
+            tmpVtm1 = _mm_load_si128(&Vtm1[i][j]);
+
             vuint8 Mtm1Plus1 = _mm_add_epi8(tmpMtm1, un);
             vuint8 Mtm1Moins1 = _mm_sub_epi8(tmpMtm1, un);
+            vuint8 NfoisOt = init_vuint8(0);
 
             vuint8 res = _mm_cmplt_epi8(_mm_sub_epi8(tmpMtm1, maxSChar), _mm_sub_epi8(tmpIt, maxSChar));
             tmpMt = _mm_or_si128(_mm_and_si128(res, Mtm1Plus1), _mm_andnot_si128(res, tmpMtm1)); //Mtm1< It
 
             res = _mm_cmpgt_epi8(_mm_sub_epi8(tmpMtm1, maxSChar), _mm_sub_epi8(tmpIt, maxSChar));
             tmpMt = _mm_or_si128(_mm_and_si128(res, Mtm1Moins1), _mm_andnot_si128(res, tmpMt)); // //Mtm1 > It
-            _mm_store_si128(&Mt[i][j], tmpMt);
-        }
-    }
 
-    for(int i = vi0; i <= vi1; i++ )//Step1
-    {
-        for(int j = vj0; j <= vj1; j++)
-        {
-            tmpIt = _mm_load_si128(&It[i][j]);
-            tmpMt = _mm_load_si128(&Mt[i][j]);
+
+            //Step 2 Calcul matrice difference |Mt-It| 
             tmpOt = _mm_min_epu8(_mm_sub_epi8(tmpMt,tmpIt), _mm_sub_epi8(tmpIt, tmpMt) ); //min(a-b,b-a) donne la valeur absolue car on peut pas avoir de valeurs negatives
-            _mm_store_si128(&vXOt[i][j], tmpOt); //Sauvegarde de l'image de difference
-        }
-    }
-
-    for(int i = vi0; i <= vi1; i++ )//Step1
-    {
-        for(int j = vj0; j <= vj1; j++)
-        {
-            tmpOt = _mm_load_si128(&vXOt[i][j]);
-            tmpVtm1 = _mm_load_si128(&Vtm1[i][j]);
-
-            vuint8 NfoisOt = init_vuint8(0);
+            
+            //Step 3 Vt Update and clamping
             for(int k = 0; k < N; k++)
             {
-                NfoisOt = _mm_adds_epi8(NfoisOt, tmpOt);
+                NfoisOt = _mm_add_epi8(NfoisOt, tmpOt);
             }
 
             vuint8 Vtm1Plus1 = _mm_add_epi8(tmpVtm1, un);
             vuint8 Vtm1Moins1 = _mm_sub_epi8(tmpVtm1, un);
-            vuint8 res = _mm_cmplt_epi8(_mm_sub_epi8(tmpVtm1, maxSChar), _mm_sub_epi8(NfoisOt, maxSChar));
+           
+            res = _mm_cmplt_epi8(_mm_sub_epi8(tmpVtm1, maxSChar), _mm_sub_epi8(NfoisOt, maxSChar));//On soustrait 128 car la comparaison est signee
             tmpVt = _mm_or_si128(_mm_and_si128(res, Vtm1Plus1), _mm_andnot_si128(res, tmpVtm1)); //Vtm1< N*Ot
 
             res = _mm_cmpgt_epi8(_mm_sub_epi8(tmpVtm1, maxSChar), _mm_sub_epi8(NfoisOt, maxSChar));
             tmpVt = _mm_or_si128(_mm_and_si128(res, Vtm1Moins1), _mm_andnot_si128(res, tmpVt)); // //Vtm1 > N* Ot
 
-
             tmpVt = _mm_max_epu8(_mm_min_epu8(tmpVt, VMAXSIMD), VMINSIMD);
 
-            _mm_store_si128(&Vt[i][j], tmpVt);
+            //Step 4: Et estimation
+             res = _mm_cmplt_epi8(_mm_sub_epi8(tmpOt,maxSChar), _mm_sub_epi8(tmpVt,maxSChar)); //Met 255 si inferieur a Vt et 0 si superieur
 
-        }
-    }
+            vuint8 dest = _mm_andnot_si128(res, pixelBlanc);//Inverse les 255 et 0 pour avoir la bonne couleur de pixel
 
-    for(int i = vi0; i <= vi1; i++ )
-    {
-        for(int j = vj0; j <= vj1; j++)
-        {
-            tmpOt = _mm_load_si128(&vXOt[i][j]);
-            tmpVt = _mm_load_si128(&Vt[i][j]);
-            vuint8 res = _mm_cmplt_epi8(_mm_sub_epi8(tmpOt,maxSChar), _mm_sub_epi8(tmpVt,maxSChar)); //Met 1 si inferieur a Vt et 0 si superieur
-
-            vuint8 dest = _mm_or_si128(_mm_and_si128(res, pixelNoir), _mm_andnot_si128(res, pixelBlanc)); //Pixel noir si res a 1 et pixel blanc si res a 0
             _mm_store_si128(&Et[i][j], dest);
-
+            _mm_store_si128(&Vt[i][j], tmpVt);
+            _mm_store_si128(&Mt[i][j], tmpMt);
         }
     }
-    free_vui8matrix(vXOt, vi0, vi1, vj0, vj1);
+
+
+
 
 
 }
