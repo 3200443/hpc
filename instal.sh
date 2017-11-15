cc68ffdbdf16614ff3603945c4329b5958acd285
3200443 <thomas.boo@etu.upmc.fr>
2017-11-15 11:01:31 +0100

    ajout instal script

---
+++ instal.sh
@@ -0,0 +1,16 @@
+rm -r car3F*
+rm -r car3S*
+rm -r obj
+
+mkdir obj
+
+mkdir car3Frame
+mkdir car3Frame3x3F
+mkdir car3Frame3x3FF
+mkdir car3Frame3x3FO
+mkdir car3Frame3x3O
+mkdir car3Frame3x3OF
+mkdir car3FrameSIMD
+mkdir car3FrameSIMD_M
+mkdir car3Sigma
+mkdir car3SigmaSIMD
