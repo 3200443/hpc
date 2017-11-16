37d20625e999cdc905a7a7e2fc87cb38119cf0a4
3200443 <thomas.boo@etu.upmc.fr>
2017-11-16 15:07:52 +0100

    ajout des bords dans la creation de matrice de test_matrice_sse2M et dans morpho

--- src/morpho_simd.c
+++ src/morpho_simd.c
@@ -351,55 +351,184 @@ void dilatation3x3_SIMD(vuint8 **It,vuint8 **It1,long vi0,long vi1,long vj0,long
 	_mm_store_si128(&It1[i][j-1],result3);
 }
 
+void dilatation3x3_SIMD_B(vuint8 **It,vuint8 **It1,long vi0,long vi1,long vj0,long vj1)
+{
+	vuint8 l1;
+	vuint8 l2;
+	vuint8 l3;
+
+	vuint8 result1,result2,result3;
+
+	vuint8 temp = init_vuint8(255);
+	vuint8 left,right;
+
+	temp; //AND DONC 255 INVARIANT
+
+
+	//premiere ligne : prologue
+	int j = vj0;
+	int i = vi0;
+
+	
+	// corps de boucle
+	for(i;i<=vi1;i++)
+	{
+		l1 = _mm_load_si128(&It[i-1][j]);
+		l2 = _mm_load_si128(&It[i+0][j]);
+		l3 = _mm_load_si128(&It[i+1][j]);
+
+		temp =  _mm_or_si128(l1,l2);
+		result1 = _mm_or_si128(temp,l3);
+
+		left = _mm_slli_si128(result1,1);
+
+		//j vaut vj0 donc on insere un zero a gauche de right
+		//right = _mm_or_si128(right,or_droit);
+		
+		j++;
+		for(j;j<=vj1;j++)
+		{
+			l1 = _mm_load_si128(&It[i-1][j]);
+			l2 = _mm_load_si128(&It[i+0][j]);
+			l3 = _mm_load_si128(&It[i+1][j]);
+
+			temp =  _mm_or_si128(l1,l2);
+			result2 = _mm_or_si128(temp,l3);
+
+			right = _mm_srli_si128(result1,1);
+			right = _mm_or_si128(right,_mm_slli_si128(result2,15)); // On complete le vecteur gauche en ajoutant a sa doite la valeur de gauvhe du vecteur suivant
+
+			result3 = _mm_or_si128(left,_mm_or_si128(right,result1));
+
+			left = _mm_slli_si128(result2,1);
+			left = _mm_or_si128(left,_mm_srli_si128(result1,15));
+
+			result1 = result2;
+
+			_mm_store_si128(&It1[i][j-1], result3);
+		}
+
+		right = _mm_srli_si128(result1,1);
+
+		result3 = _mm_or_si128(left,_mm_or_si128(right,result1)); // Valeur finale de IT1[i][j-1]
+
+		_mm_store_si128(&It1[i][j-1],result3);
+
+		j= vj0;
+	}
+}
+
 void fermeture3x3_SIMD(vuint8 **It,vuint8 **It1,long vi0,long vi1,long vj0,long vj1)
 {
-	vuint8 ** vXVt = vui8matrix(vi0-2, vi1+2, vj0-1, vj1+1);
-	dilatation3x3_SIMD(It,vXVt,vi0,vi1,vj0,vj1);
-	erosion3x3_SIMD(vXVt,It1,vi0,vi1,vj0,vj1);
-	free_vui8matrix(vXVt,vi0-2, vi1+2, vj0-1, vj1+1);
+	vuint8 ** vXVt = vui8matrix(vi0-1, vi1+1, vj0-1, vj1+1);
+	dilatation3x3_SIMD_B(It,vXVt,vi0,vi1,vj0,vj1);
+	erosion3x3_SIMD_B(vXVt,It1,vi0,vi1,vj0,vj1);
+	free_vui8matrix(vXVt,vi0-1, vi1+1, vj0-1, vj1+1);
 }
 
 void ouverture3x3_SIMD(vuint8 **It,vuint8 **It1,long vi0,long vi1,long vj0,long vj1)
 {
-	vuint8 ** vXVt = vui8matrix(vi0, vi1, vj0, vj1);
-	erosion3x3_SIMD(It,vXVt,vi0,vi1,vj0,vj1);
-	dilatation3x3_SIMD(vXVt,It1,vi0,vi1,vj0,vj1);
-	free_vui8matrix(vXVt,vi0, vi1, vj0, vj1);
+	vuint8 ** vXVt = vui8matrix(vi0-1, vi1+1, vj0-1, vj1+1);
+	erosion3x3_SIMD_B(It,vXVt,vi0,vi1,vj0,vj1);
+	dilatation3x3_SIMD_B(vXVt,It1,vi0,vi1,vj0,vj1);
+	free_vui8matrix(vXVt,vi0-1, vi1+1, vj0-1, vj1+1);
 }
 
 void erosion5x5_SIMD(vuint8 **It,vuint8 **It1,long vi0,long vi1,long vj0,long vj1)
 {
-	vuint8 ** vXVt = vui8matrix(vi0, vi1, vj0, vj1);
-	erosion3x3_SIMD(It,vXVt,vi0,vi1,vj0,vj1);
-	erosion3x3_SIMD(vXVt,It1,vi0,vi1,vj0,vj1);
-	free_vui8matrix(vXVt,vi0, vi1, vj0, vj1);
+	vuint8 ** vXVt = vui8matrix(vi0-1, vi1+1, vj0-1, vj1+1);
+	erosion3x3_SIMD_B(It,vXVt,vi0,vi1,vj0,vj1);
+	erosion3x3_SIMD_B(vXVt,It1,vi0,vi1,vj0,vj1);
+	free_vui8matrix(vXVt,vi0-1, vi1+1, vj0-1, vj1+1);
 }
 
 void dilatation5x5_SIMD(vuint8 **It,vuint8 **It1,long vi0,long vi1,long vj0,long vj1)
 {
-	vuint8 ** vXVt = vui8matrix(vi0, vi1, vj0, vj1);
-	dilatation3x3_SIMD(It,vXVt,vi0,vi1,vj0,vj1);
-	dilatation3x3_SIMD(vXVt,It1,vi0,vi1,vj0,vj1);
-	free_vui8matrix(vXVt,vi0, vi1, vj0, vj1);
+	vuint8 ** vXVt = vui8matrix(vi0-1, vi1+1, vj0-1, vj1+1);
+	dilatation3x3_SIMD_B(It,vXVt,vi0,vi1,vj0,vj1);
+	dilatation3x3_SIMD_B(vXVt,It1,vi0,vi1,vj0,vj1);
+	free_vui8matrix(vXVt,vi0-1, vi1+1, vj0-1, vj1+1);
 }
 
 void fermeture5x5_SIMD(vuint8 **It,vuint8 **It1,long vi0,long vi1,long vj0,long vj1)
 {
-	vuint8 ** vXVt = vui8matrix(vi0, vi1, vj0, vj1);
+	vuint8 ** vXVt = vui8matrix(vi0-1, vi1+1, vj0-1, vj1+1);
 	dilatation5x5_SIMD(It,vXVt,vi0,vi1,vj0,vj1);
 	erosion5x5_SIMD(vXVt,It1,vi0,vi1,vj0,vj1);
-	free_vui8matrix(vXVt,vi0, vi1, vj0, vj1);
+	free_vui8matrix(vXVt,vi0-1, vi1+1, vj0-1, vj1+1);
 }
 
 void ouverture5x5_SIMD(vuint8 **It,vuint8 **It1,long vi0,long vi1,long vj0,long vj1)
 {
-	vuint8 ** vXVt = vui8matrix(vi0, vi1, vj0, vj1);
+	vuint8 ** vXVt = vui8matrix(vi0-1, vi1+1, vj0-1, vj1+1);
 	erosion5x5_SIMD(It,vXVt,vi0,vi1,vj0,vj1);
 	dilatation5x5_SIMD(vXVt,It1,vi0,vi1,vj0,vj1);
-	free_vui8matrix(vXVt,vi0, vi1, vj0, vj1);
+	free_vui8matrix(vXVt,vi0-1, vi1+1, vj0-1, vj1+1);
 }
 
-void fermeture5x5_SIMD_O(vuint8 **It,vuint8 **It1,long vi0,long vi1,long vj0,long vj1)
+void dilatation3x3_SIMD_BD(vuint8 **It,vuint8 **It1,long vi0,long vi1,long vj0,long vj1)
 {
+	vuint8 l1;
+	vuint8 l2;
+	vuint8 l3;
 
+	vuint8 result1,result2,result3;
+
+	vuint8 temp = init_vuint8(255);
+	vuint8 left,right;
+
+	temp; //AND DONC 255 INVARIANT
+
+
+	//premiere ligne : prologue
+	int j = vj0;
+	int i = vi0;
+
+	
+	// corps de boucle
+	for(i;i<=vi1;i++)
+	{
+		l1 = _mm_load_si128(&It[i-1][j]);
+		l2 = _mm_load_si128(&It[i+0][j]);
+		l3 = _mm_load_si128(&It[i+1][j]);
+
+		temp =  _mm_or_si128(l1,l2);
+		result1 = _mm_or_si128(temp,l3);
+
+		left = _mm_slli_si128(result1,1);
+
+		//j vaut vj0 donc on insere un zero a gauche de right
+		//right = _mm_or_si128(right,or_droit);
+		
+		j++;
+		for(j;j<=vj1;j++)
+		{
+			l1 = _mm_load_si128(&It[i-1][j]);
+			l2 = _mm_load_si128(&It[i+0][j]);
+			l3 = _mm_load_si128(&It[i+1][j]);
+
+			temp =  _mm_or_si128(l1,l2);
+			result2 = _mm_or_si128(temp,l3);
+
+			right = _mm_srli_si128(result1,1);
+			right = _mm_or_si128(right,_mm_slli_si128(result2,15)); // On complete le vecteur gauche en ajoutant a sa doite la valeur de gauvhe du vecteur suivant
+
+			result3 = _mm_or_si128(left,_mm_or_si128(right,result1));
+
+			left = _mm_slli_si128(result2,1);
+			left = _mm_or_si128(left,_mm_srli_si128(result1,15));
+
+			result1 = result2;
+
+			_mm_store_si128(&It1[i][j-1], result3);
+		}
+
+		right = _mm_srli_si128(result1,1);
+
+		result3 = _mm_or_si128(left,_mm_or_si128(right,result1)); // Valeur finale de IT1[i][j-1]
+
+		_mm_store_si128(&It1[i][j-1],result3);
+
+		j= vj0;
+	}
 }
--- src/test_mouvement_SSE2.c
+++ src/test_mouvement_SSE2.c
@@ -256,8 +256,8 @@ void test_routine_FrameDifference_SSE2M(int seuil)
     vuint8 ** vIt = vui8matrix_s(nrl, nrh, ncl, nch);
     vuint8 ** vEt = vui8matrix_s(nrl, nrh, ncl, nch);
     //
-    vuint8 ** vEt1 = vui8matrix_s(nrl, nrh, ncl, nch);
-    vuint8 ** vEt2 = vui8matrix_s(nrl, nrh, ncl, nch);
+    vuint8 ** vEt1 = vui8matrix_s(nrl-1, nrh+1, ncl-1, nch+1);
+    vuint8 ** vEt2 = vui8matrix_s(nrl-1, nrh+1, ncl-1, nch+1);
     //
     vuint8 seuilSIMD = init_vuint8(seuil); //Copie du seuil dans un vecteur SIMD
 
@@ -287,8 +287,8 @@ void test_routine_FrameDifference_SSE2M(int seuil)
     free_vui8matrix(vIt, vi0, vi1, vj0, vj1);
     free_vui8matrix(vEt, vi0, vi1, vj0, vj1);
     //
-    free_vui8matrix(vEt1, vi0, vi1, vj0, vj1);
-    free_vui8matrix(vEt2, vi0, vi1, vj0, vj1);
+    free_vui8matrix(vEt1, vi0-1, vi1+1, vj0-1, vj1+1);
+    free_vui8matrix(vEt2, vi0-1, vi1+1, vj0-1, vj1+1);
     //
 
 
